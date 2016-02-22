e113452d0eac81bf8376c34338c73de1d1add3a7
Farid Jafaroff's Alpha Blog <placeboy.jafaroff@gmail.com>
2016-02-22 12:45:50 +0000

    Sign up from user UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_many :articles
-    
+    has_secure_password
     before_save { self.email = email.downcase }
     validates :username, presence: true,
                 uniqueness: { case_sensitive: false },
@@ -11,5 +11,5 @@ class User < ActiveRecord::Base
                 uniqueness: { case_sensitive: false },
                 format: { with: VALID_EMAIL_REGEX }
                 
-    has_secure_password
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   
   root "pages#home"
   get 'about', to: "pages#about"
-
+  get 'signup', to: "users#new"
+  
+  resources :users, except: [:new]
   resources :articles
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
